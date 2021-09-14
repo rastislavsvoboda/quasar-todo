733c5f335198e075196fff6c08c918a7255ddd7e
Rastislav Svoboda <rastislav.svoboda@gmail.com>
2021-09-14 22:29:27 +0200

    in progress

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,112 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+      </q-toolbar>
+      <div class="q-px-lg q-pt-xl q-mb-md">
+        <div class="text-h3">Todo</div>
+        <div class="text-subtitle1">{{ todaysDate }}</div>
+      </div>
+      <q-img src="~assets/mountains.jpg" class="header-image absolute-top" />
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      :width="250"
+      :breakpoint="600"
+    >
+      <q-scroll-area
+        style="
+          height: calc(100% - 192px);
+          margin-top: 192px;
+          border-right: 1px solid #ddd;
+        "
+      >
+        <q-list padding>
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="list" />
+            </q-item-section>
+
+            <q-item-section> Todo </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="help" />
+            </q-item-section>
+
+            <q-item-section> Help </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+
+      <q-img
+        class="absolute-top"
+        src="~assets/mountains.jpg"
+        style="height: 192px"
+      >
+        <div class="absolute-bottom bg-transparent">
+          <q-avatar size="56px" class="q-mb-sm">
+            <img
+              src="https://s.gravatar.com/avatar/c9903c55ae43a43ad62f57d4b71384c8?s=80"
+            />
+          </q-avatar>
+          <div class="text-weight-bold">Rastislav Svoboda</div>
+          <div>@RastoSvoboda</div>
+        </div>
+      </q-img>
+    </q-drawer>
+
+    <q-page-container>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { date } from "quasar";
+
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {},
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    const timeStamp = Date.now();
+    const todaysDate = date.formatDate(timeStamp, "dddd D MMMM YYYY");
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+      todaysDate,
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+.header-image {
+  height: 100%;
+  z-index: -1;
+  opacity: 0.2;
+  filter: grayscale(100%);
+}
+</style>
